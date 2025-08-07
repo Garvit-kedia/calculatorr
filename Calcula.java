a06b723289b61058995fbf0a66f70ac7c1772b62
Garvit Kedia <garvitkedia2@gmail.com>
2025-08-07 15:15:14 +0530

    Add addition method

--- Calcula.java
+++ Calcula.java
@@ -1,8 +1,14 @@
 package calculatorr;
 
 public class Calcula {
+    public int  add( int a,int b){
+            return a+b;
+
+        }
     public static void main(String[] args) {
         System.out.println("heloo");
+
+        
     }
     
 }
